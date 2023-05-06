a6facf78ea46f1be22cd3d1df941c18259a6f71e
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2023-05-06 18:21:22 -0500

    add total values row

--- src/rendering.ts
+++ src/rendering.ts
@@ -1,6 +1,53 @@
 import type { TransactionSchema } from './schemas';
 import { format } from 'date-fns';
 
+interface TableRowInput {
+    date?: Date;
+    name: string;
+    email: string;
+    description: string;
+    id: string;
+    currencyCode: string;
+    amountGross: number;
+    amountFees: number;
+    amountNet: number;
+    isBold: boolean;
+}
+
+export function getTableRowHtml({
+    date,
+    name,
+    email,
+    description,
+    id,
+    currencyCode,
+    amountGross,
+    amountFees,
+    amountNet,
+    isBold,
+}: TableRowInput) {
+    const fontClass = isBold ? 'font-bold' : 'font-normal';
+    const row = `
+         <tr class="border-b">
+            <th class="p-2 ${fontClass} text-sm">${
+        date ? format(date, 'MM/dd/yyyy') : ''
+    }</th>
+            <th class="p-2 ${fontClass} text-sm">
+                <div>${name}</div>
+                <div>${email}</div>
+            </th>
+            <th class="p-2 ${fontClass} text-sm">
+                <div>${description}</div>
+                <div>${id.length > 0 ? `ID: ${id}` : ''}</div>
+            </th>
+            <th class="p-2 ${fontClass} text-sm">${currencyCode}</th>
+            <th class="p-2 ${fontClass} text-sm">${amountGross.toFixed(2)}</th>
+            <th class="p-2 ${fontClass} text-sm">${amountFees.toFixed(2)}</th>
+            <th class="p-2 ${fontClass} text-sm">${amountNet.toFixed(2)}</th>
+        </tr>`;
+    return row;
+}
+
 export function getTableHtml(transactions: TransactionSchema[], title: string) {
     const headerHtml = `
     <thead>
@@ -15,7 +62,13 @@ export function getTableHtml(transactions: TransactionSchema[], title: string) {
         </tr>
     </thead>
     `;
+    let totalFees = 0;
+    let totalNet = 0;
+    let totalGross = 0;
     const bodyRows = transactions.map((transaction) => {
+        totalFees += transaction.amountFees;
+        totalGross += transaction.amountGross;
+        totalNet += transaction.amountNet;
         let desc = transaction.description;
         if (!desc) {
             if (transaction.amountGross < 0) {
@@ -25,34 +78,32 @@ export function getTableHtml(transactions: TransactionSchema[], title: string) {
             }
         }
 
-        const row = `
-         <tr class="border-b">
-            <th class="p-2 font-normal text-sm">${format(
-                transaction.date,
-                'MM/dd/yyyy'
-            )}</th>
-            <th class="p-2 font-normal text-sm">
-                <div>${transaction.customerName ?? ''}</div>
-                <div>${transaction.customerEmail ?? ''}</div>
-            </th>
-            <th class="p-2 font-normal text-sm">
-                <div>${desc}</div>
-                <div>ID: ${transaction.id}</div>
-            </th>
-            <th class="p-2 font-normal text-sm">${transaction.currencyCode}</th>
-            <th class="p-2 font-normal text-sm">${transaction.amountGross.toFixed(
-                2
-            )}</th>
-            <th class="p-2 font-normal text-sm">${transaction.amountFees.toFixed(
-                2
-            )}</th>
-            <th class="p-2 font-normal text-sm">${transaction.amountNet.toFixed(
-                2
-            )}</th>
-        </tr>`;
-
-        return row;
+        return getTableRowHtml({
+            id: transaction.id,
+            date: transaction.date,
+            name: transaction.customerName ?? '',
+            email: transaction.customerEmail ?? '',
+            description: desc,
+            amountFees: transaction.amountFees,
+            amountGross: transaction.amountGross,
+            amountNet: transaction.amountNet,
+            currencyCode: transaction.currencyCode,
+            isBold: false,
+        });
     });
+    bodyRows.push(
+        getTableRowHtml({
+            id: '',
+            name: '',
+            description: '',
+            amountNet: totalNet,
+            amountFees: totalFees,
+            amountGross: totalGross,
+            email: '',
+            currencyCode: '',
+            isBold: true,
+        })
+    );
 
     const html = `
     <div class="my-10">
